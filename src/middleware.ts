e57139578cfe698662a85600bf616afd13deaba4
AlexanderMolano <81302682+AlexanderMolano@users.noreply.github.com>
2025-12-05 19:02:44 -0500

    nuam & MUI Pro build 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
+});
 
 export const config = {
   matcher: [
     "/PageGestAcepCes/:path*",
     "/PageIngresoAcep/:path*",
     "/SelectorRole/:path*",
+    "/DetalleGralOperEsp/:path*",
     // Agrega aquí todas las rutas que quieres proteger
   ],
 };
